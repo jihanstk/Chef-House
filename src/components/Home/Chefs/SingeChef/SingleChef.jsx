9b17aa97e95f44b27284363830bedb31c51afe75
jihanstkm <jihanstk@gmail.com>
2023-05-04 22:40:04 +0600

    add chef details

--- src/components/Home/Chefs/SingeChef/SingleChef.jsx
+++ src/components/Home/Chefs/SingeChef/SingleChef.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import SingleChefRecipe from "./SingleChefRecipe/SingleChefRecipe";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { FaHeart } from "react-icons/fa";
 
 const SingleChef = () => {
   const singleChefRec = useLoaderData();
@@ -10,11 +11,11 @@ const SingleChef = () => {
   return (
     <div className="bg-yellow-200/20">
       <ToastContainer />
-      <div className="w-full h-80 bg-blue-700 relative mb-32">
+      <div className="w-full h-80 bg-blue-700 relative mb-44">
         <h1 className="text-3xl text-center pt-20 text-white font-semibold">
           Here is chef Delicious recipe
         </h1>
-        <div className=" absolute -bottom-20 md:left-2/3 left-32">
+        <div className=" absolute -bottom-40  md:left-2/3 left-32">
           <img
             className="w-32 h-32 rounded-full border-8 border-white"
             src={singleChefRec.photo}
@@ -23,6 +24,19 @@ const SingleChef = () => {
           <h3 className="text-2xl font-bold text-left -ml-8">
             {singleChefRec.chefName}
           </h3>
+          <p className="text-left -ml-8">
+            <strong>{singleChefRec.yearsOfExperience}</strong> Year Of
+            Experience
+          </p>
+          <p className="text-left -ml-8">
+            Made <strong>{singleChefRec.numberOfRecipes}</strong> number's Of
+            Recipe
+          </p>
+          <p className=" text-left -ml-8 flex items-center text-lg font-semibold">
+            <span className="mr-2">Likes : </span>
+            <FaHeart className="mr-2 text-red-600"></FaHeart>{" "}
+            <span>{singleChefRec.likes}</span>
+          </p>
         </div>
       </div>
       <h1 className="text-4xl font-bold text-center mb-10">
